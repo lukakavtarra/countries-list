f7a11c96e3b81d52ad07faa6e9226c18d7ad7fa8
Luka <lukakavtaradze@gmail.com>
2022-08-07 20:47:28 +0400

    countries list

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
+import {Component} from 'react';
+import Countries from './components/Countries';
+class App extends Component {
+  state = {
+    countries: [],
+    searchField: ''
+  }
+  componentDidMount() {
+   
+    fetch('https://restcountries.com/v3.1/all')
+      .then((response) => response.json())
+      .then((users) =>
+        this.setState(
+          () => {
+            return { countries: users };
+          },
+          () => {
+            // console.log(this.state)
+          }
+        )
+      );
+  }
 
-function App() {
+  //Get Key's While Changing Input On Search
+ onSearchChange = (event) => {
+  const searchField = event.target.value.toLowerCase();
+  this.setState(() => {return {searchField}})  
+}
+
+render() {
+  const clickOnACountry = (item) => {
+    console.log(item)
+    const chosenCountry = countries.filter(country => {
+      return country.cca2.includes(item)
+    })
+    console.log(chosenCountry);
+  }
+  const {countries, searchField} = this.state;
+  const { onSearchChange, test } = this;
+  // Get Filtered Countrys After 
+  const filteredCountries = countries.filter(country => {
+    return country.name.official.toLowerCase().includes(searchField)
+  })
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Countries 
+      sendCountries = {filteredCountries} 
+      clickedCountry = {clickOnACountry}
+      searchButton = {onSearchChange} />
     </div>
   );
 }
-
+}
 export default App;
---
+++ src/components/Countries.jsx
@@ -0,0 +1,75 @@
+import { Component } from 'react'
+import {IoReturnDownBack} from 'react-icons/io5'
+
+class Countries extends Component {
+  render() {
+    const {sendCountries, clickedCountry, searchButton} = this.props;
+if(window.location.pathname == '/'){
+    console.log(sendCountries)
+    return (
+        <div className="">
+       <input onChange={(event) => searchButton(event) } type="text" />
+
+        <div className='countries__list' >
+
+        {sendCountries.map((item) => {
+          return (
+            
+              <div title={item.name.official} onClick={() => clickedCountry(item.cca2)} className='countries' key={item.cca2}>
+              <a href={item.cca2}>
+                <img title={item.name.official} className='flag' src={item.flags.svg} alt="flag" />
+                <h1 title={item.name.official} className='name'>{item.name.official}</h1>
+                <p title={item.name.official} className="population">Population: {item.population}</p>
+                <p title={item.name.official} className="region">Region: {item.region}</p>
+                <p title={item.name.official} className="capital">Capital: {item.capital}</p>
+              </a>
+
+              </div>
+          )
+        })}
+        </div>
+        </div>
+    )
+}else {
+    const showCountry = window.location.pathname.replace('/',"");
+    // console.log(showCountry)
+    const chosenCountry = sendCountries.filter(country => {
+        return country.cca2.includes(showCountry)
+      })
+      console.log(chosenCountry)
+
+      if(chosenCountry.length){
+        const getNative = chosenCountry[0].name.nativeName[Object.keys(chosenCountry[0].name.nativeName)[0]]
+        const getLanguages = chosenCountry[0].languages[Object.keys(chosenCountry[0].languages)[0]]
+        const getCurrencies = chosenCountry[0].currencies[Object.keys(chosenCountry[0].currencies)[0]]
+    return (
+        <div className="chosen">
+        <a href="/"><IoReturnDownBack className='back' /> back</a>
+        <div className="chosenCountryDiv">
+            <img className='chosenCountryImg' src={chosenCountry[0].flags.svg} alt="flag" />
+            <div>
+            <h1 className='chosenCountryName'>{chosenCountry[0].name.common}</h1>
+                <div className="informatic__page">
+                    <div className="">
+                        <p className="chosenNativeName"><span>Native Name:</span> {getNative.official}</p>
+                        <p className="chosenPopulation"><span>Population:</span>  {chosenCountry[0].population}</p>
+                        <p className="chosenRegion"><span>Region:</span>  {chosenCountry[0].region}</p>
+                        <p className="chosenSubRegion"><span>Sub Region:</span> {chosenCountry[0].population}</p>
+                        <p className="chosenCapital"><span>Capital:</span>  {chosenCountry[0].capital[0]}</p>
+                    </div>
+                    <div className="">
+                        <p className="domain"><span>Top Level Domain:</span> {chosenCountry[0].tld[0]}</p>
+                        <p className="domain"><span>Currencies:</span>  {getCurrencies.name} {getCurrencies.symbol}</p>
+                        <p className="domain"><span>Languages:</span>  {getLanguages}</p>
+                    </div>
+            </div>
+            </div>
+        </div>
+        </div>
+    )
+}
+}
+  }
+}
+
+export default Countries
